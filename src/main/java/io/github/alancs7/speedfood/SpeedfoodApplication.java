f7035c183459fc393a331bcd17cd022742c961fd
Alan Silva <alanclash2541@gmail.com>
2023-12-01 00:26:01 -0300

    feat(SpeedFood): Project created

---
+++ src/main/java/io/github/alancs7/speedfood/SpeedfoodApplication.java
@@ -0,0 +1,13 @@
+package io.github.alancs7.speedfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedfoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/alancs7/speedfood/SpeedfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alancs7.speedfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedfoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
