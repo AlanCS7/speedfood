a0a78bb67b3974efa44ec9771caa4e574e6dee94
Alan Carlos <alanclash2541@gmail.com>
2023-12-10 14:06:31 -0300

    feat(Restaurante): criando uma fabrica de specification

---
+++ src/main/java/io/github/alancs7/speedfood/infrastructure/repository/spec/RestauranteSpecs.java
@@ -0,0 +1,17 @@
+package io.github.alancs7.speedfood.infrastructure.repository.spec;
+
+import io.github.alancs7.speedfood.domain.model.Restaurante;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.math.BigDecimal;
+
+public class RestauranteSpecs {
+
+    public static Specification<Restaurante> comFreteGratis() {
+        return (root, query, builder) -> builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+    }
+
+    public static Specification<Restaurante> comNomeSemelhante(String nome) {
+        return (root, query, builder) -> builder.like(root.get("nome"), "%" + nome + "%");
+    }
+}
