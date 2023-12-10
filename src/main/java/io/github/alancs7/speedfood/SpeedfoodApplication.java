6910b864289a807be19014d3e90a10454f248d5d
Alan Carlos <alanclash2541@gmail.com>
2023-12-10 14:05:12 -0300

    feat(SpeedFood): Criando repositorio customizado

--- src/main/java/io/github/alancs7/speedfood/SpeedfoodApplication.java
+++ src/main/java/io/github/alancs7/speedfood/SpeedfoodApplication.java
@@ -1,9 +1,12 @@
 package io.github.alancs7.speedfood;
 
+import io.github.alancs7.speedfood.infrastructure.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class SpeedfoodApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/io/github/alancs7/speedfood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -0,0 +1,31 @@
+package io.github.alancs7.speedfood.infrastructure.repository;
+
+import io.github.alancs7.speedfood.domain.repository.CustomJpaRepository;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+public class CustomJpaRepositoryImpl<T, ID> extends
+        SimpleJpaRepository<T, ID> implements CustomJpaRepository<T, ID> {
+
+    private final EntityManager entityManager;
+
+    public CustomJpaRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<T> buscarPrimeiro() {
+        var jpql = "from " + getDomainClass().getName();
+
+        T entity = entityManager.createQuery(jpql, getDomainClass())
+                .setMaxResults(1)
+                .getSingleResult();
+
+        return Optional.ofNullable(entity);
+    }
+}
