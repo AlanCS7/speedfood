5f7201b2a514595ba4dbd883b098e9f5b4a31283
Alan Carlos <alanclash2541@gmail.com>
2024-01-16 20:22:00 -0300

    feat: Criando novas classes DTO e configurando no modelmapper

--- src/main/java/io/github/alancs7/speedfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/io/github/alancs7/speedfood/core/modelmapper/ModelMapperConfig.java
@@ -1,8 +1,10 @@
 package io.github.alancs7.speedfood.core.modelmapper;
 
+import io.github.alancs7.speedfood.api.model.dto.EnderecoDto;
 import io.github.alancs7.speedfood.api.model.input.CidadeInput;
 import io.github.alancs7.speedfood.api.model.input.RestauranteInput;
 import io.github.alancs7.speedfood.domain.model.Cidade;
+import io.github.alancs7.speedfood.domain.model.Endereco;
 import io.github.alancs7.speedfood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +18,15 @@ public class ModelMapperConfig {
         ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.createTypeMap(RestauranteInput.class, Restaurante.class)
-                .addMapping(RestauranteInput::getCozinha, (restaurante, value) -> restaurante.getCozinha().setId((Long) value));
+                .<Long>addMapping(RestauranteInput::getCozinha, (restauranteDest, value) -> restauranteDest.getCozinha().setId(value));
 
         modelMapper.createTypeMap(CidadeInput.class, Cidade.class)
-                .addMapping(CidadeInput::getEstado, (cidade, value) -> cidade.getEstado().setId((Long) value));
+                .<Long>addMapping(CidadeInput::getEstado, (cidadeDest, value) -> cidadeDest.getEstado().setId(value));
+
+        modelMapper.createTypeMap(Endereco.class, EnderecoDto.class)
+                .<String>addMapping(
+                        enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                        (enderecoDtoDest, value) -> enderecoDtoDest.getCidade().setEstado(value));
 
         return modelMapper;
     }
