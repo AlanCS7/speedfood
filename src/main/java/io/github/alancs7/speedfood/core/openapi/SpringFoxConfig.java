3dff1fd223bc0dfeb73465b2f58e3962aa61f70f
Alan Carlos <alanclash2541@gmail.com>
2024-04-06 22:20:37 -0300

    feat: Gerando a documentação da API em HTML com Swagger UI e SpringFox e Descrevendo informações da API na documentação

--- src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
@@ -2,7 +2,11 @@ package io.github.alancs7.speedfood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,7 +19,19 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("io.github.alancs7.speedfood.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SpeedFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1.0")
+                .contact(new Contact("SpeedFood", "https://github.com/AlanCS7", "alancss.contact@gmail.com"))
                 .build();
     }
+
 }
