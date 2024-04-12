ebe0e014537cde84fda0797397f1927444e894e0
Alan Carlos <alanclash2541@gmail.com>
2024-04-12 01:21:00 -0300

    feat: Descrevendo códigos de status de respostas de forma global

--- src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
@@ -3,16 +3,22 @@ package io.github.alancs7.speedfood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -26,11 +32,63 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.basePackage("io.github.alancs7.speedfood.api"))
                 .paths(PathSelectors.any())
                 .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponses())
+                .globalResponses(HttpMethod.POST, globalPostPutResponses())
+                .globalResponses(HttpMethod.PUT, globalPostPutResponses())
+                .globalResponses(HttpMethod.DELETE, globalDeleteResponses())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
 
-    public ApiInfo apiInfo() {
+    private List<Response> globalGetResponses() {
+        return List.of(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que seja aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    private List<Response> globalPostPutResponses() {
+        return List.of(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Requisição inválida (erro do cliente)")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno no servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+                        .description("Recurso não possui representação que seja aceita pelo consumidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+                        .description("Requisição recusada porque o corpo está em um formato não suportado")
+                        .build()
+        );
+    }
+
+    private List<Response> globalDeleteResponses() {
+        return List.of(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                        .description("Requisição inválida (erro do cliente)")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do servidor")
+                        .build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("SpeedFood API")
                 .description("API aberta para clientes e restaurantes")
