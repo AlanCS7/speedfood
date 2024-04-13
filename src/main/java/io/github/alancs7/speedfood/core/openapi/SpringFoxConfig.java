e297b2218af5444c0b311d48e93c6d15aebd45a5
Alan Carlos <alanclash2541@gmail.com>
2024-04-13 18:07:10 -0300

    feat: Ignorando tipos de parâmetros de operações na documentação

--- src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/io/github/alancs7/speedfood/core/openapi/SpringFoxConfig.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.request.ServletWebRequest;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.*;
 import springfox.documentation.schema.AlternateTypeRules;
@@ -48,6 +49,7 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponses())
                 .additionalModels(typeResolver.resolve(ApiError.class))
                 .directModelSubstitute(Pageable.class, PageableOpenApi.class)
+                .ignoredParameterTypes(ServletWebRequest.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(Page.class, CozinhaDto.class),
                         CozinhaDtoOpenApi.class
